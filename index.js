c26a8850087bb2c07541a4cc26540c3116cda300
Abhishek V <88491499+abhiofcl@users.noreply.github.com>
2024-01-28 00:34:58 +0530

    modifications done

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
+var oldValue = "";
+var isClicked = false;
+
 function btnClick(val) {
+  var operand = document.getElementById("screen").value;
+  oldValue += val;
+  if (val == "+" || val == "-" || val == "*" || val == "/") {
+    document.getElementById("screen").value = "";
+    isClicked = true;
+    return;
+  }
+  if (isClicked) {
+    document.getElementById("screen").value = val;
+    isClicked = false;
+    // document.getElementById("screen").value = "";
+  } else {
+    document.getElementById("screen").value += val;
+  }
+}
+
+function normal(val) {
   document.getElementById("screen").value += val;
 }
 
 function clearScreen() {
   document.getElementById("screen").value = "";
+  oldValue = "";
+  isClicked = false;
 }
+
 function equalClick() {
   var expression = document.getElementById("screen").value;
-  var result = eval(expression);
+  try {
+    var result = eval(oldValue);
+    document.getElementById("screen").value = result;
+  } catch {
+    document.getElementById("screen").value = "Invalid operation";
+  }
   console.log(result);
-  document.getElementById("screen").value = result;
 }
