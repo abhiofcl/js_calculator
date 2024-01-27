7034fda514370439fabcc04fbf083d9e2fc8955d
Abhishek V <88491499+abhiofcl@users.noreply.github.com>
2024-01-27 23:41:36 +0530

    basic operations can be done

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function btnClick(val) {
-  document.getElementsByClassName("screen").value += val;
+  document.getElementById("screen").value += val;
+}
+
+function clearScreen() {
+  document.getElementById("screen").value = "";
+}
+function equalClick() {
+  var expression = document.getElementById("screen").value;
+  var result = eval(expression);
+  console.log(result);
+  document.getElementById("screen").value = result;
 }
